4baaac8ef8a7cdd7eacbcbd30fcfe5a7d806aa86
Denis <d.blinkovv@yandex.ru>
2021-03-29 17:08:35 +0300

    switched address to host on and rewrite requirements

--- courier_service/api/__main__.py
+++ courier_service/api/__main__.py
@@ -1,13 +1,11 @@
-from http import HTTPStatus
-
-from aiohttp.web import run_app
+import aiohttp.web
 
 from courier_service.api.app import create_app
 
 
 def main():
     app = create_app()
-    run_app(app)
+    aiohttp.web.run_app(app, host='0.0.0.0', port=8080)
 
 
 if __name__ == '__main__':
